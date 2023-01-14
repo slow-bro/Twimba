321148b211bfbcf690e302244d0717d52bd43c05
stevie <116691349+slow-bro@users.noreply.github.com>
2023-01-13 18:11:51 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [   
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `images/troll.jpg`,
+        profilePic: `troll.jpg`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -13,19 +13,19 @@ export const tweetsData = [
     },    
     {
         handle: `@Elon ✅`,
-        profilePic: `images/musk.png`,
+        profilePic: `musk.png`,
         likes: 6500,
         retweets: 234,
         tweetText: `Just wasted $44b on Twitter..`,
         replies: [
                   {
                 handle: `@TomCruise ✅`,
-                profilePic: `images/tcruise.png`,
+                profilePic: `tcruise.png`,
                 tweetText: `Good luck brother!`,
             },
                   {
                 handle: `@ChuckNorris ✅`,
-                profilePic: `images/chucknorris.jpeg`,
+                profilePic: `chucknorris.jpeg`,
                 tweetText: `Yikes 😴`,
             },
         ],
@@ -35,19 +35,19 @@ export const tweetsData = [
     },
         {
         handle: `@NoobCoder12`,
-        profilePic: `images/flower.png`,
+        profilePic: `flower.png`,
         likes: 10,
         retweets: 3,
         tweetText: `Are you a coder if you only know HTML?`,
         replies: [
             {
                 handle: `@StackOverflower ☣️`,
-                profilePic: `images/overflow.png`,
+                profilePic: `overflow.png`,
                 tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
             },
             {
                 handle: `@YummyCoder64`,
-                profilePic: `images/love.png`,
+                profilePic: `love.png`,
                 tweetText: `You are wonderful just as you are! ❤️`,
             },
         ],
